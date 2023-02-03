fb733c036f773698e1d63f3d41e295e346e79b83
xnick <xnick@users.noreply.github.com>
2023-02-03 15:00:55 +0200

    Replace the dot with a dash so that it can be removed in isFieldMatch.

--- apps/browser/src/autofill/services/autofill-constants.ts
+++ apps/browser/src/autofill/services/autofill-constants.ts
@@ -100,7 +100,7 @@ export class CreditCardAutoFillConstants {
     "carte-credit",
     "num-carte",
     "cb-num",
-    "card.pan",
+    "card-pan",
   ];
 
   static readonly CardNumberFieldNameValues: string[] = [

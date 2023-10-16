e00a6a3bcac46f2defa27bceab277a24e24c7e54
Jacob Fink <jfink@bitwarden.com>
2023-10-16 13:39:40 -0400

    fix imports

--- apps/web/src/app/auth/auth.module.ts
+++ apps/web/src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 
+import { CoreAuthModule } from "./core/core.module";
 import { AuthSettingsModule } from "./settings/settings.module";
 
+
 @NgModule({
-  imports: [AuthSettingsModule],
+  imports: [AuthSettingsModule, CoreAuthModule],
   declarations: [],
   providers: [],
-  exports: [AuthSettingsModule],
+  exports: [AuthSettingsModule, CoreAuthModule],
 })
 export class AuthModule {}

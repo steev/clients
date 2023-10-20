64026d3ab818e721280cddb2b5054f1af86453cc
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-10-19 21:28:17 -0400

    Rework fromChromeEvent
    Rework fromChromeEvent so we have to lie to TS less and
    remove unneeded generics. I did this by caring less about
    the function and more about the parameters only.
    
    Co-authored-by: Matt Gibson <MGibson1@users.noreply.github.com>

--- apps/browser/src/platform/browser/from-chrome-event.ts
+++ apps/browser/src/platform/browser/from-chrome-event.ts
@@ -1,20 +1,20 @@
 import { Observable } from "rxjs";
 
-export function fromChromeEvent<
-  T extends (...args: unknown[]) => void,
-  P extends unknown[] = Parameters<T>
->(target: chrome.events.Event<T>): Observable<P> {
-  return new Observable<P>((subscriber) => {
-    const handler = (...args: P) => {
+export function fromChromeEvent<T extends unknown[]>(
+  event: chrome.events.Event<(...args: T) => void>
+): Observable<T> {
+  return new Observable<T>((subscriber) => {
+    const handler = (...args: T) => {
       if (chrome.runtime.lastError) {
         subscriber.error(chrome.runtime.lastError);
         return;
       }
+
       subscriber.next(args);
     };
-    // TODO: why do I need to fake this
-    target.addListener(handler as T);
 
-    return () => target.removeListener(handler as T);
+    event.addListener(handler);
+
+    return () => event.removeListener(handler);
   });
 }

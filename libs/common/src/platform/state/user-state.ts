7026e3adda0d3ccb843b34b881458f741d231a87
Matt Gibson <mgibson@bitwarden.com>
2023-10-30 17:20:35 -0400

    Allow for update logic in state update callbacks

--- libs/common/src/platform/state/user-state.ts
+++ libs/common/src/platform/state/user-state.ts
@@ -2,6 +2,8 @@ import { Observable } from "rxjs";
 
 import { UserId } from "../../types/guid";
 
+import { StateUpdateOptions } from "./state-update-options";
+
 import { DerivedStateDefinition, DerivedUserState } from ".";
 
 /**
@@ -13,16 +15,33 @@ export interface UserState<T> {
   /**
    * Updates backing stores for the active user.
    * @param configureState function that takes the current state and returns the new state
+   * @param options Defaults to @see {module:state-update-options#DEFAULT_OPTIONS}
+   * @param options.shouldUpdate A callback for determining if you want to update state. Defaults to () => true
+   * @param options.combineLatestWith An observable that you want to combine with the current state for callbacks. Defaults to null
+   * @param options.msTimeout A timeout for how long you are willing to wait for a `combineLatestWith` option to complete. Defaults to 1000ms. Only applies if `combineLatestWith` is set.
+
    * @returns The new state
    */
-  readonly update: (configureState: (state: T) => T) => Promise<T>;
+  readonly update: <TCombine>(
+    configureState: (state: T, dependencies: TCombine) => T,
+    options?: StateUpdateOptions<T, TCombine>
+  ) => Promise<T>;
   /**
    * Updates backing stores for the given userId, which may or may not be active.
    * @param userId the UserId to target the update for
    * @param configureState function that takes the current state for the targeted user and returns the new state
+   * @param options Defaults given by @see {module:state-update-options#DEFAULT_OPTIONS}
+   * @param options.shouldUpdate A callback for determining if you want to update state. Defaults to () => true
+   * @param options.combineLatestWith An observable that you want to combine with the current state for callbacks. Defaults to null
+   * @param options.msTimeout A timeout for how long you are willing to wait for a `combineLatestWith` option to complete. Defaults to 1000ms. Only applies if `combineLatestWith` is set.
+
    * @returns The new state
    */
-  readonly updateFor: (userId: UserId, configureState: (state: T) => T) => Promise<T>;
+  readonly updateFor: <TCombine>(
+    userId: UserId,
+    configureState: (state: T, dependencies: TCombine) => T,
+    options?: StateUpdateOptions<T, TCombine>
+  ) => Promise<T>;
 
   /**
    * Creates a derives state from the current state. Derived states are always tied to the active user.

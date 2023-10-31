3a7512b60d1711c8666af32f74544313107151b8
Jake Fink <jfink@bitwarden.com>
2023-10-31 12:16:50 -0400

    remove changes to core organization module

--- apps/web/src/app/admin-console/organizations/core/core-organization.module.ts
+++ apps/web/src/app/admin-console/organizations/core/core-organization.module.ts
@@ -1,11 +1,4 @@
 import { NgModule } from "@angular/core";
 
-import { AccountRecoveryService } from "../members/services/account-recovery/account-recovery.service";
-
-@NgModule({
-  imports: [],
-  declarations: [],
-  providers: [AccountRecoveryService],
-  exports: [],
-})
+@NgModule({})
 export class CoreOrganizationModule {}

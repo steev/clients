95404d0cb38b4dd50a500562d3361931b55096c8
Jared Snider <jsnider@bitwarden.com>
2023-11-03 16:38:47 -0400

    PM-2041 - Recreate  BaseLoginViaWebAuthnComponent and then web implementation of it.

--- apps/web/src/app/auth/login/login.module.ts
+++ apps/web/src/app/auth/login/login.module.ts
@@ -6,11 +6,22 @@ import { SharedModule } from "../../../app/shared";
 
 import { LoginDecryptionOptionsComponent } from "./login-decryption-options/login-decryption-options.component";
 import { LoginViaAuthRequestComponent } from "./login-via-auth-request.component";
+import { LoginViaWebAuthnComponent } from "./login-via-webauthn/login-via-webauth.component";
 import { LoginComponent } from "./login.component";
 
 @NgModule({
   imports: [SharedModule, CheckboxModule],
-  declarations: [LoginComponent, LoginViaAuthRequestComponent, LoginDecryptionOptionsComponent],
-  exports: [LoginComponent, LoginViaAuthRequestComponent, LoginDecryptionOptionsComponent],
+  declarations: [
+    LoginComponent,
+    LoginViaAuthRequestComponent,
+    LoginDecryptionOptionsComponent,
+    LoginViaWebAuthnComponent,
+  ],
+  exports: [
+    LoginComponent,
+    LoginViaAuthRequestComponent,
+    LoginDecryptionOptionsComponent,
+    LoginViaWebAuthnComponent, // TODO: why does this need to be exported?
+  ],
 })
 export class LoginModule {}

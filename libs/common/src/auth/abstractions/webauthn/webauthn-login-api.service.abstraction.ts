5b208de3cd83a86ba9e8ca536750477d15543916
Jared Snider <jsnider@bitwarden.com>
2023-11-07 15:19:37 -0500

    PM-2041 - Remove unused param from WebAuthnLoginApiServiceAbstraction as we aren't supporting non-discoverable passkeys for MVP

--- libs/common/src/auth/abstractions/webauthn/webauthn-login-api.service.abstraction.ts
+++ libs/common/src/auth/abstractions/webauthn/webauthn-login-api.service.abstraction.ts
@@ -1,5 +1,5 @@
 import { CredentialAssertionOptionsResponse } from "../../services/webauthn-login/response/credential-assertion-options.response";
 
 export class WebAuthnLoginApiServiceAbstraction {
-  getCredentialAssertionOptions: (email?: string) => Promise<CredentialAssertionOptionsResponse>;
+  getCredentialAssertionOptions: () => Promise<CredentialAssertionOptionsResponse>;
 }
